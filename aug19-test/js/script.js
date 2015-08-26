8e1f46beb544c7716d78bc093eec023360114eb4
geerios95 <geerios95@gmail.com>
2015-08-26 15:30:24 -0700

    add files

---
+++ aug19-test/js/script.js
@@ -0,0 +1,29 @@
+/* put your javascript here */
+/*var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.520618, lng: -73.552457},
+    scrollwheel: false,
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  });
+}*/
+//$big: giraffe;
+var $sizers = document.getElementsByTagName('button');
+var $size = document.getElementsByTagName('body')[0];
+var $default = '100%';
+for(var $i = 0; $i < $sizers.length; $i++){
+	$size.style.fontSize = $default;
+	$sizers[$i].addEventListener('click', function(){
+		// write stuff
+		var $getBtn = this.innerHTML;
+		if($getBtn == ' + '){
+			console.log($default);
+			//$newdefault = $default + '10%';
+			$size.style.fontSize = "200%";
+		}else{
+			//$newdefault = $default - '10%';
+			$size.style.fontSize = "100%";
+		}
+	});
+};
