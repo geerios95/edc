52ca97516535f8eeb4ec70c0d08a5de1f09526f5
geerios95 <geerios95@gmail.com>
2015-09-14 16:48:45 -0700

    more files ive been/currently working on

--- portfolio/gulpfile.js
+++ portfolio/gulpfile.js
@@ -1,51 +1,53 @@
-var gulp 		= require('gulp'),
-	gutil		= require('gulp-util'),
-	browserify	= require('gulp-browserify'),
-	compass		= require('gulp-compass'),
-	autoprefixer= require('gulp-autoprefixer'),
-	connect		= require('gulp-connect');
-
-var htmlSources	= ['*.html'];
-var	scssSources	= ['scss/*.scss'];
-var jsSources	= ['js/*.js'];
-
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	connect = require('gulp-connect'),
+	plumber = require('gulp-plumber');
+var htmlSources = ['*.html'];
+var scssSources = ['scss/*.scss'];
+var jsSources = ['js/*.js'];
+var onError = function (err) {  
+	gutil.beep();
+	console.log(err);
+	this.emit('end');
+};
 gulp.task('html', function(){
 	gulp.src(htmlSources)
 		.pipe(connect.reload());
 });
-
-gulp.task('compass',function(){
+gulp.task('compass', function(){
 	gulp.src(scssSources)
+		.pipe(plumber({ errorHandler: onError }))
 		.pipe(compass({
-			sass:'scss/',
-			image:'img/',
+			sass: 'scss/',
+			image: 'img/',
 			style: 'expanded'
-		}).on('error',gutil.log))
+		}))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
-			cascade: true
+			cascade: true	
 		}))
 		.pipe(gulp.dest('css/'))
 		.pipe(connect.reload());
 });
-
-gulp.task('js',function(){
+gulp.task('js', function(){
 	gulp.src(jsSources)
+		.pipe(plumber({ errorHandler: onError }))
 		.pipe(browserify())
 		.pipe(connect.reload());
 });
-
-gulp.task('connect',function(){
+gulp.task('connect', function(){
 	connect.server({
-		root:'',
-		livereload:true
-	});
-})
-
-gulp.task('watch',function(){
-	gulp.watch(htmlSources,['html']);
-	gulp.watch(scssSources,['compass']);
-	gulp.watch(jsSources,['js']);
-})
-
-gulp.task('default',['watch','html','compass','js','connect']);
+		root: '',
+		livereload: true	
+	});	
+});
+gulp.task('watch', function(){
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(scssSources, ['compass']);
+	gulp.watch(jsSources, ['js']);
+});
+gulp.task('default', ['watch', 'html', 'compass', 'js', 'connect']);
+Add Comment
